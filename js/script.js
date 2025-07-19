d0c3d79b56d0a750898d5572b2d8ccef91eb2c3d
kritikumar1 <kritikumarpradhan999@gmail.com>
2025-07-19 11:10:23 +0530

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const container = document.querySelector('.container');
-const loginlink = document.querySelector('.signinlink');
-const registerlink = document.querySelector('.signuplink');
-registerlink.addEventListener('click', () => {
-    container.classList.add('active');
-})
-loginlink.addEventListener('click', () => {
-    container.classList.remove('active');
-})
